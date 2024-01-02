c3b9a8d3c3daa0d68879a7f4e47c2cfd7e42e1ac
icep0ps <tapsmuko@gmail.com>
2024-01-02 14:19:00 +0200

    feat: basic page layout :sparkles:

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import BankRate from '@/components/bankRate';
+import CurrencyConverter from '@/components/currency_converter/currencyConverter';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+      <BankRate />
+      <CurrencyConverter />
+    </main>
   );
 }
---
+++ components/bankRate.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const BankRate: FC = () => {
+  return (
+    <section className="flex flex-col items-center">
+      <p>Todays offical bank rate</p>
+      <h1>6 192.40 ZWL</h1>
+    </section>
+  );
+};
+
+export default BankRate;
