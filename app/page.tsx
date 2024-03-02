c0866c5dbba88a727c940305cff174065ef8279c
icep0ps <tapsmuko@gmail.com>
2024-03-02 21:14:24 +0200

    feat: added error handing :sparkles:

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,31 @@ import Rate from '@/components/rate';
 import Rates from '@/components/rates';
 
 const getOfficalRate = async () => {
-  const data = await fetch('http://localhost:3000/api/rates/offical', {
-    method: 'GET',
-    cache: 'no-store',
-  });
+  try {
+    const data = await fetch('http://localhost:3000/api/rates/offical', {
+      method: 'GET',
+      cache: 'no-store',
+    });
 
-  const rates = await data.json();
-  return rates as currency;
+    const rates = await data.json();
+    return rates as currency;
+  } catch (error) {
+    throw new Error('Error fetching offical rate: ' + error);
+  }
 };
 
 const getRates = async () => {
-  const data = await fetch('http://localhost:3000/api/rates', {
-    method: 'GET',
-    cache: 'no-store',
-  });
+  try {
+    const data = await fetch('http://localhost:3000/api/rates', {
+      method: 'GET',
+      cache: 'no-store',
+    });
 
-  const rates = await data.json();
-  return rates as currency[];
+    const rates = await data.json();
+    return rates as currency[];
+  } catch (error) {
+    throw new Error('Error fetching rates: ' + error);
+  }
 };
 
 export default async function Home() {
