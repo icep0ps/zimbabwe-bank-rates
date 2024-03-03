6dac0851c4c523291819f8efa7b5063d7edf43c8
icep0ps <tapsmuko@gmail.com>
2024-03-04 01:46:02 +0200

    bugfix: pdfjs-dist, migrated to pdf2json :bug:

--- utils/extractor/extractor.js
+++ utils/extractor/extractor.js
@@ -1,13 +1,28 @@
+import fs from 'fs';
+import data from './rates.json' assert { type: 'json' };
+import PDFParser from 'pdf2json';
 import Database from '../database/database.js';
-import pdf_table_extractor from 'pdf-table-extractor';
 
 class Extractor {
   static read() {
-    pdf_table_extractor('utils/extractor/rates.pdf', Extractor.success, Extractor.error);
+    const pdfParser = new PDFParser();
+
+    pdfParser.on('pdfParser_dataError', (errData) => console.error(errData.parserError));
+    pdfParser.on('pdfParser_dataReady', (pdfData) => {
+      fs.writeFile('./utils/extractor/rates.json', JSON.stringify(pdfData), (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          Extractor.success();
+        }
+      });
+    });
+
+    pdfParser.loadPDF('./utils/extractor/rates.pdf');
   }
 
-  static success(result) {
-    const ratesdata = Extractor.genarateRates(result);
+  static success() {
+    const ratesdata = Extractor.genarateRates();
     Database.create.rates(ratesdata);
   }
 
@@ -15,26 +30,44 @@ class Extractor {
     console.log('Error: ' + err);
   }
 
-  static genarateRates(result) {
-    const rates = {};
-    const rows = result.pageTables[0].tables;
+  static genarateRates() {
+    const rates = [];
+    let groupedRates = [];
+    const currency = new RegExp(/^[A-Z]{3}(%2F[A-Z]+)?$/);
+    const whitespace = new RegExp(/%20/);
+    const textNodes = data.Pages[0].Texts.filter((node) => !whitespace.test(node.R.T));
 
-    rows
-      .slice(4, result.pageTables[0].tables.length)
-      .filter((data) => data !== '')
-      .forEach((currency) => {
-        Object.assign(rates, {
-          [currency[0]]: {
-            bid: Number(currency[2].replace(',', '')),
-            ask: Number(currency[3].replace(',', '')),
-            mid_rate: Number(currency[4].replace(',', '')),
-            bid_zwl: Number(currency[5].replace(',', '')),
-            ask_zwl: Number(currency[6].replace(',', '')),
-            mid_zwl: Number(currency[7].replace(',', '')),
-          },
-        });
+    const ungroupedRates = data.Pages[0].Texts.map((node) => {
+      return Number(node.R[0].T.replace('%2C', ''));
+    }).filter((item) => typeof item === 'number' && !isNaN(item) && item != 0);
+
+    for (let i = 41; i >= 0; i--) {
+      groupedRates.push(ungroupedRates.splice(0, Math.ceil(ungroupedRates.length / i)));
+    }
+
+    const filteredCurrencies = textNodes
+      .map((node) => {
+        if (currency.test(node.R[0].T)) {
+          return node.R[0].T;
+        }
+      })
+      .filter((currency) => currency != undefined);
+
+    const currencies = filteredCurrencies.slice(7, filteredCurrencies.length);
+
+    for (let currency = 0; currency < currencies.length; currency++) {
+      const rate = groupedRates[currency];
+      rates.push({
+        [currencies[currency]]: {
+          bid: rate[0],
+          ask: rate[1],
+          mid_rate: rate[2],
+          bid_zwl: rate[3],
+          ask_zwl: rate[4],
+          mid_zwl: rate[5],
+        },
       });
-
+    }
     return rates;
   }
 }
