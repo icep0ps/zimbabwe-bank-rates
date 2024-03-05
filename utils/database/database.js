7cf2a7fef9e8f633b91d8fd13e4dff9e82462d6e
icep0ps <tapsmuko@gmail.com>
2024-03-05 23:09:38 +0200

    feat: fully migrated to postgres :sparkles:

--- utils/database/database.js
+++ utils/database/database.js
@@ -3,68 +3,71 @@ import 'dotenv/config';
 import pkg from 'pg';
 const { Client } = pkg;
 
-const client = new Client({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
-
 class Database {
   static async connect() {
-    const con = await client.connect();
-    return con;
+    const client = new Client({
+      host: process.env.HOST,
+      user: process.env.DB_USER,
+      database: process.env.DATABASE,
+      password: process.env.PASSWORD,
+      port: process.env.DB_PORT,
+    });
+
+    await client.connect().catch((error) => {
+      console.log('Error connecting to db: ' + error.message);
+    });
+
+    return client;
   }
 
   static async query(query, params) {
-    const connection = Database.connect();
-    return new Promise((res, rej) => {
-      connection.query(query, params, function (err, results, fields) {
-        if (err) {
-          rej(err);
-        } else {
-          res(results);
-        }
-        connection.end();
-      });
+    const connection = await Database.connect();
+    return await connection.query(query, params).then((res) => {
+      return res.rows;
     });
   }
 
   static get = {
     async rates() {
-      return await Database.query(
-        'SELECT * FROM `rates` WHERE `date_published` =  "2024-01-03";'
+      const rates = await Database.query(
+        "SELECT * FROM rates WHERE date_published =  '2024-03-04'"
       );
+      return rates;
     },
 
     async offical(currency) {
-      return await Database.query(
-        'SELECT * FROM `rates` WHERE `date_published` = "2024-01-03" AND `currency` = ? ;',
+      const offical = await Database.query(
+        "SELECT * FROM rates WHERE date_published = '2024-03-04' AND currency = $1",
         [currency]
       );
+      return offical;
     },
   };
 
   static create = {
-    rates(rates) {
-      const connection = Database.connect();
+    async rates(rates) {
+      const connection = await Database.connect();
 
       for (const [currency, rate_values] of Object.entries(rates)) {
         const { bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl } = rate_values;
 
-        connection.execute(
-          'INSERT INTO `rates` (currency, bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl) VALUES (?,?,?,?,?,?,?);',
-          [currency, bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl],
-          function (err, results, fields) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log('created rates for ' + currency);
-            }
-          }
-        );
-        connection.end();
+        await connection
+          .query(
+            'INSERT INTO rates (currency, bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl) VALUES ($1,$2,$3,$4,$5,$6,$7)',
+            [
+              currency.trim().replace('%2F', '/'),
+              bid,
+              ask,
+              mid_rate,
+              bid_zwl,
+              ask_zwl,
+              mid_zwl,
+            ]
+          )
+          .then(() => {
+            console.log('Created Rates successfully');
+            connection.end();
+          });
       }
     },
   };
