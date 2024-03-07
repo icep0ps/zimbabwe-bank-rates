a8fbf7633c681dacfa84908697a86d255470ea1a
icep0ps <tapsmuko@gmail.com>
2024-03-07 23:55:21 +0200

    test: added genarateRates tests :rotating_light:

--- tests/utils/extractor.test.js
+++ tests/utils/extractor.test.js
@@ -1,5 +1,25 @@
-describe('methods return correct values', () => {
-  test('getting object with offical rate', () => {
-    expect('124').toBe('asdasdas');
+import Extractor from '@/utils/extractor/extractor.js';
+
+describe('generateRates return correct values', () => {
+  test('returns an object', () => {
+    const rates = Extractor.genarateRates();
+    expect(typeof rates == 'object').toBe(true);
+  });
+
+  test('currency is mapped to object', () => {
+    const rates = Extractor.genarateRates();
+    expect(typeof rates['USD'] == 'object').toBe(true);
+  });
+
+  test('currency value has correct stucture', () => {
+    const rates = Extractor.genarateRates();
+    expect(rates['USD']).toStrictEqual({
+      bid: expect.any(Number),
+      ask: expect.any(Number),
+      mid_rate: expect.any(Number),
+      bid_zwl: expect.any(Number),
+      ask_zwl: expect.any(Number),
+      mid_zwl: expect.any(Number),
+    });
   });
 });
