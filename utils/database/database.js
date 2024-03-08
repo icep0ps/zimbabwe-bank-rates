96fb486eb5b2ac3e9cad41f3c5883bb5b9a5e023
icep0ps <tapsmuko@gmail.com>
2024-03-09 00:21:38 +0200

    refactor: migrated to postgres :hammer:

--- utils/database/database.js
+++ utils/database/database.js
@@ -1,11 +1,9 @@
 import 'dotenv/config';
-
-import pkg from 'pg';
-const { Client } = pkg;
+import postgres from 'postgres';
 
 class Database {
   static async connect() {
-    const client = new Client({
+    const client = postgres({
       host: process.env.HOST,
       user: process.env.DB_USER,
       database: process.env.DATABASE,
@@ -13,62 +11,30 @@ class Database {
       port: process.env.DB_PORT,
     });
 
-    await client.connect().catch((error) => {
-      console.log('Error connecting to db: ' + error.message);
-    });
-
     return client;
-  }
-
-  static async query(query, params) {
-    const connection = await Database.connect();
-    return await connection.query(query, params).then((res) => {
-      return res.rows;
-    });
   }
 
   static get = {
     async rates() {
-      const rates = await Database.query(
-        "SELECT * FROM rates WHERE date_published =  '2024-03-04'"
-      );
-      return rates;
+      const sql = await Database.connect();
+      return sql`SELECT * FROM rates WHERE date_published =  '2024-03-04'`;
     },
 
     async offical(currency) {
-      const offical = await Database.query(
-        "SELECT * FROM rates WHERE date_published = '2024-03-04' AND currency = $1",
-        [currency]
-      );
-      return offical;
+      const sql = await Database.connect();
+      return sql`SELECT * FROM rates WHERE date_published = '2024-03-04' AND currency=${currency}`;
     },
   };
 
   static create = {
     async rates(rates) {
+      const sql = await Database.connect();
       console.log('Creaing rates in database');
-      const connection = await Database.connect();
 
       for (const [currency, rate_values] of Object.entries(rates)) {
         const { bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl } = rate_values;
-
-        await connection
-          .query(
-            'INSERT INTO rates (currency, bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl) VALUES ($1,$2,$3,$4,$5,$6,$7)',
-            [
-              currency.trim().replace('%2F', '/'),
-              bid,
-              ask,
-              mid_rate,
-              bid_zwl,
-              ask_zwl,
-              mid_zwl,
-            ]
-          )
-          .then(() => {
-            console.log('Created Rates successfully');
-            connection.end();
-          });
+        const currencyName = currency.trim().replace('%2F', '/');
+        return sql`INSERT INTO rates (currency, bid, ask, mid_rate, bid_zwl, ask_zwl, mid_zwl) VALUES (${currencyName},${bid},${ask},${mid_rate},${bid_zwl},${ask_zwl},${mid_zwl}) returning *`;
       }
     },
   };
