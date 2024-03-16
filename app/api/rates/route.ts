ab374ad3ef274974d04b5059550465406d469425
icep0ps <tapsmuko@gmail.com>
2024-03-16 23:03:06 +0200

    bugfix: not returning all rates :bug:

--- app/api/rates/route.ts
+++ app/api/rates/route.ts
@@ -3,5 +3,5 @@ import Database from '@/services/database/database';
 export const dynamic = 'force-dynamic'; // defaults to auto
 export async function GET(request: Request) {
   const rates = await Database.get.rates();
-  return rates[0] === undefined ? Response.json([]) : Response.json(rates[0]);
+  return rates[0] === undefined ? Response.json([]) : Response.json(rates);
 }
