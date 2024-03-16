15effee35ef2cff6bd516df08dc3908b803cdbf9
icep0ps <tapsmuko@gmail.com>
2024-03-16 22:50:20 +0200

    bugfix: script not exiting :bug:

--- services/rates/script.js
+++ services/rates/script.js
@@ -1,4 +1,11 @@
 import Getpdf from './getpdf.js';
 import Extractor from './extractor/extractor.js';
 
-Extractor.success().then(() => console.log('done'));
+Extractor.success()
+  .then(() => {
+    console.log('Created rates succesfully');
+    process.exit(1);
+  })
+  .catch((error) => {
+    throw new Error('Failed to create rates: ' + error.message);
+  });
