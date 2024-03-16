bf35ed4be22f22dbc3af4e9d66a9b9345a3df20c
icep0ps <tapsmuko@gmail.com>
2024-03-17 00:03:20 +0200

    bugfix: build errors #1 due to rbz route :bug:

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,4 @@
 'use client';
-
-import { useEffect } from 'react';
-
 export default function Error({
   error,
   reset,
@@ -9,10 +6,6 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
   return (
     <div>
       <h2>Something went wrong! {error.message}</h2>
