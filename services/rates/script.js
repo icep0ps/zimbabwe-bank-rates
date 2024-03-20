9a66571b31199162becd85c1f06a6f5ff11748ae
icep0ps <tapsmuko@gmail.com>
2024-03-21 01:02:50 +0200

    bugfix: exiting with error status on success🐛

--- services/rates/script.js
+++ services/rates/script.js
@@ -1,10 +1,9 @@
-import Getpdf from './getpdf.js';
 import Extractor from './extractor/extractor.js';
 
 Extractor.success()
   .then(() => {
     console.log('Created rates succesfully');
-    process.exit(1);
+    process.exit(0);
   })
   .catch((error) => {
     throw new Error('Failed to create rates: ' + error.message);
