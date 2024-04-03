84d4deebb8d7ffad22821435d717a117373fe1df
icep0ps <tapsmuko@gmail.com>
2024-04-03 16:09:45 +0200

    chore: cleanup

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -4,23 +4,10 @@ type Props = {};
 
 const Navigation = (props: Props) => {
   return (
-    <nav className="flex justify-between  p-5  w-full items-center  ">
+    <nav className="flex justify-between w-full items-center  ">
       <Link href={'/#rate'} className="normal-case">
         Zimbabwe bank rates
       </Link>
-
-      <ul className="flex gap-5  items-center">
-        <li className="border-b-4 border-accent">
-          <Link href={'/#exchange_rates'}>Home</Link>
-        </li>
-        <li>
-          <Link href={'/#exchange_rates'}>Exchange rates</Link>
-        </li>
-
-        <li>
-          <Link href={'/faq'}>About</Link>
-        </li>
-      </ul>
     </nav>
   );
 };
