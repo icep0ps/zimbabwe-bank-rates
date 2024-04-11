b06cf246f2425adfbf83621dcee25e9b6b14ebe9
icep0ps <tapsmuko@gmail.com>
2024-04-11 20:08:40 +0200

    bugfix: user clash in env virables  #2 :bug:

--- services/providers/locals.js
+++ services/providers/locals.js
@@ -11,11 +11,11 @@ class Locals {
     }
 
     return {
-      host: process.env.HOST,
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      database: process.env.DATABASE,
-      password: process.env.PASSWORD,
-      port: process.env.PORT,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
     };
   }
 }
