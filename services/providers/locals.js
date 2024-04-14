f69d5cc6aa8be00a87c929625cc4a71b1e4394cd
icep0ps <tapsmuko@gmail.com>
2024-04-14 23:13:25 +0200

    chore: change dir

---
+++ services/providers/locals.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV === 'production') dotenv.config({ path: '.env.production' });
+else dotenv.config({ path: '.env.local' });
+
+class Locals {
+  static config() {
+    for (const [key, value] of Object.entries(process.env)) {
+      if (value === undefined)
+        throw new Error(`Please set ${key} in environment variables file`);
+    }
+
+    console.log({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
+
+    return {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    };
+  }
+}
+
+export default Locals;
