f18c060c4989cd0ab293704fbec0eae1129afabd
icep0ps <tapsmuko@gmail.com>
2024-04-26 10:17:37 +0200

    feat: two way conversion :sparkles:

--- components/converter/converter.tsx
+++ components/converter/converter.tsx
@@ -18,21 +18,31 @@ const CurrencyConverter: FC<Props> = (props) => {
   const [primaryAmout, setPriamaryAmount] = useState<string>(
     secondaryCurrency.mid_zwl.toString()
   );
-  const [secondaryAmount, setSecondaryAmount] = useState<string>('1.00');
+  const [secondaryAmount, setSecondaryAmount] = useState<string>(
+    Number(props.rate.ask).toFixed(2).toString()
+  );
 
   const handleConversion = useCallback(
-    () => (parseFloat(primaryAmout) / secondaryCurrency.mid_zwl).toFixed(2),
+    (type: 'priamary' | 'secondary', amount: string) => {
+      if (type == 'priamary')
+        return (parseFloat(amount) / secondaryCurrency.mid_zwl).toFixed(2);
+
+      if (type == 'secondary')
+        return (props.rate.mid_zwl * parseFloat(amount)).toFixed(2);
+
+      return '0';
+    },
     [primaryAmout, secondaryCurrency.mid_zwl]
   );
 
   useEffect(() => {
     //Doing this inorder to use the updated primary amount state.
-    setSecondaryAmount(handleConversion());
-  }, [primaryAmout, secondaryCurrency.currency]);
+    setSecondaryAmount(handleConversion('priamary', primaryAmout));
+  }, [secondaryCurrency.currency]);
 
   return (
     <div className="flex flex-col justify-between items-start w-3/4 gap-5">
-      <span className="w-full border-input border rounded-lg py-3 px-2 flex">
+      <span className="w-full focus-within:border-accent border rounded-lg py-3 px-2 flex">
         <span className="bg-border p-2 rounded-lg text-sm mr-3">ZiG</span>
         <input
           min={'0'}
@@ -42,6 +52,7 @@ const CurrencyConverter: FC<Props> = (props) => {
           className="bg-background outline-none"
           onChange={(event) => {
             setPriamaryAmount(parseFloat(event.target.value).toString());
+            setSecondaryAmount(handleConversion('priamary', event.target.value));
           }}
         />
       </span>
@@ -55,6 +66,7 @@ const CurrencyConverter: FC<Props> = (props) => {
           value: secondaryAmount,
           setAmount: (event) => {
             setSecondaryAmount(event.target.value);
+            setPriamaryAmount(handleConversion('secondary', event.target.value));
           },
         }}
         rates={props.rates}
