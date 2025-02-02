e65e7ea7cfd7691ec17d1f333d200c6e01d258de
icep0ps <tapsmuko@gmail.com>
2025-02-02 14:48:55 +0200

    tests: put tests in same folder as thier modules

--- types.ts
+++ types.ts
@@ -1,13 +1,13 @@
 export type currency = {
   currency: string;
-  bid: number;
-  ask: number;
-  mid_rate: number;
-  bid_zwl: number;
-  ask_zwl: number;
-  mid_zwl: number;
+  bid: string;
+  ask: string;
+  mid_rate: string;
+  bid_zwl: string;
+  ask_zwl: string;
+  mid_zwl: string;
   date_published: Date;
   previous_date_published: Date;
-  previous_mid_rate_zwl: number;
+  previous_mid_rate_zwl: string;
   name: string;
 };
